d2d215d81f762d779b5b9e13df88e40b3ab66722
Carlos Loya <cloyajim@gmail.com>
2023-02-16 00:35:00 -0600

    recyclerView parent

--- app/src/main/java/com/example/nestedrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/nestedrecyclerview/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.nestedrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.nestedrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var parentAdapter: ParentMusicGroupAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupRecycleView()
+        getData()
+    }
+
+    private fun setupRecycleView() {
+        parentAdapter = ParentMusicGroupAdapter()
+        binding.rvParent.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = parentAdapter
+        }
+    }
+
+    private fun getData() {
+
     }
 }
